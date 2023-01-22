2fe3f2d1697ccbdfaf605a6f0cf399975ea09212
izzetcansuzen <izzetcansuzen@gmail.com>
2023-01-22 20:49:17 +0300

    form validate system completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,102 @@ import {useState} from "react";
 import FormStatus from "./components/FormStatus.jsx";
 
 export default function App(){
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+        password_verification: "",
+        isAccepted: ""
+    });
+
+    const [formStatus, setFormStatus] = useState({
+        status: undefined,
+        message: ""
+    })
+
+    function handleFormChange(){
+        const {name, value, type, checked} = event.target;
+        setFormData(prevState => ({
+            ...prevState,
+            [name]: type == "checkbox" ? checked : value
+        }))
+    }
+
+    function handleFormSubmit(){
+        event.preventDefault();
+        // console.log(formData);
+        if(formData.email && formData.password && formData.password_verification && formData.isAccepted){
+            setFormStatus({
+                    status: true,
+                    message: "Form Başarıyla Gönderildi"
+            });
+        }
+        if(formData.password !== formData.password_verification){
+            setFormStatus({
+                status: false,
+                message: "Şifreler birbiri ile eşleşmiyor"
+            });
+        }
+        if(!formData.email || !formData.password || !formData.password_verification){
+            setFormStatus({
+                status: false,
+                message: "Değerler boş olmamalıdır"
+            });
+        }
+        if(!formData.isAccepted){
+            setFormStatus({
+                status: false,
+                message: "Termleri kabul etmelisiniz"
+            });
+        }
+    }
+
     return (
         <div className="container">
-            <FormStatus />
-            <form action="" method="post">
+            {
+                formStatus.status !== undefined ?
+                    <FormStatus
+                        formStatus={formStatus}
+                    /> :
+                    ""
+            }
+            <form action="" onSubmit={handleFormSubmit}>
                 <div className="container_form">
                     <label htmlFor="email">Email</label>
-                    <input type="email" placeholder="SENİ" id="email" className="input"/>
+                    <input type="email"
+                           placeholder="email"
+                           id="email"
+                           className="input"
+                           name="email"
+                           onChange={handleFormChange}
+                    />
                 </div>
                 <div className="container_form">
                     <label htmlFor="password">Password</label>
-                    <input type="password" placeholder="ÇOOOOOOKK" id="password" className="input"/>
+                    <input type="password"
+                           placeholder="password"
+                           id="password"
+                           className="input"
+                           name="password"
+                           onChange={handleFormChange}
+                    />
                 </div>
                 <div className="container_form">
                     <label htmlFor="password_verification">Password Verification</label>
-                    <input type="password" placeholder="SEVİYORUMMMM" id="password_verification" className="input"/>
+                    <input type="password"
+                           placeholder="password verification"
+                           id="password_verification"
+                           className="input"
+                           name="password_verification"
+                           onChange={handleFormChange}
+                    />
                 </div>
                 <div className="container_form container_checkbox">
-                    <input type="checkbox" id="isAccepted" className="input_checkbox"/>
+                    <input type="checkbox"
+                           id="isAccepted"
+                           className="input_checkbox"
+                           name="isAccepted"
+                           onChange={handleFormChange}
+                    />
                     <label htmlFor="isAccepted">Accept Terms</label>
                 </div>
                 <button type="submit">Submit</button>
--- src/components/FormStatus.jsx
+++ src/components/FormStatus.jsx
@@ -2,10 +2,10 @@
     //True ise, Yeşil olup yazı "Form Başarıyla Gönderildi"
     //False ise, Kırmızı olup yazı "Gönderilemedi"
 //mesajları verilmeli
-export default function FormStatus(){
+export default function FormStatus(props){
     return (
-        <div className="formStatus">
-            <span>Form Başarıyla Gönderildi</span>
+        <div className={props.formStatus.status ? "formStatus" : "unsuccessful"}>
+            <span>{props.formStatus.message}</span>
         </div>
     )
 }
